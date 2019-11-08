4ddd685f1a97cbf5dff17bdba49b664b3b1a59e3
夏泽宇 <1215884463@qq.com>
2019-11-08 16:41:53 +0800

    1

--- src/main/java/com/xiazeyu/express/ExpressRunner.java
+++ src/main/java/com/xiazeyu/express/ExpressRunner.java
@@ -9,6 +9,8 @@ public class ExpressRunner {
     public static void main(String[] args) {
         //Word[] parse = WordSplit.parse("2*3/(2-1)+3*(4-1)", KeyWordDefine.splitWords);
         Word[] parse = WordSplit.parse("(1 + 4) * 3 + 10 / 5", KeyWordDefine.splitWords);
+
+
     }
 
 }
